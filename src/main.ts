e1ac0d79c6741aa40034131f2d7be5a9e50219a9
lodmev <lodmev@gmail.com>
2024-09-17 13:38:19 +0300

    feat: add swagger package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const PORT = Number(process.env.PORT) || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Movies service')
+    .setVersion('0.1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/ui', app, document);
+  await app.listen(PORT, () => console.log(`server start on port ${PORT}`));
 }
 bootstrap();
