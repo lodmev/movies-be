4660d8706db6bbbf93d53775b19721788bcb75c3
lodmev <lodmev@gmail.com>
2024-09-17 15:45:21 +0300

    feat: implement prisma module and add nestjs config

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
 
 async function bootstrap() {
   const PORT = Number(process.env.PORT) || 3000;
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
   const config = new DocumentBuilder()
     .setTitle('Movies service')
     .setVersion('0.1.0')
