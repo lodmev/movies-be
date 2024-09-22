dc0e99061697a9ef144fd545d2b645b2aff22423
lodmev <lodmev@gmail.com>
2024-09-22 11:32:22 +0300

    fix: attempt get user pass then user not found

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,10 +14,16 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
   async login(email: string, password: string) {
+    const error = new UnauthorizedException(
+      'No users with such email or password',
+    );
     const user = await this.prisma.users.findUnique({ where: { email } });
+    if (!user) {
+      throw error;
+    }
     const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!user || !isPasswordValid) {
-      throw new UnauthorizedException('No users with such email or password');
+    if (!isPasswordValid) {
+      throw error;
     }
     return {
       accessToken: this.jwtService.sign({ userId: user.id }),
